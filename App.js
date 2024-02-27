09fcca25b9adf421f6703d4f1c2bb9faa16f5275
ThomasSpare <t.spare.jkpg@gmail.com>
2024-02-28 00:52:59 +0100

    Initial Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>SIGNUM</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridTemplateRows: '1fr 2fr repeat(2, 1fr)',
+    gridColumnGap: 0,
+    gridRowGap: 0,
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    div1: { gridArea: '2/1/3/2', backgroundColor: 'red' },
+    div2: { gridArea: '2 / 2 / 3 / 3', },
+    div3: { gridArea: '2 /3 / 3 / 4', },
+    
   },
-});
+  text: {
+    color: 'cyan',
+    marginBottom: 750,
+    marginLeft: 280,
+    fontFamily: '',
+    fontStyle: 'bold',
+    },
+  }
+)
